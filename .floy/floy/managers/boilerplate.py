bd77ecef27476d3cc3a7abb8a4f67e50edd1e5b7
Christian Kaisermann <christian@kaisermann.me>
2017-02-03 15:01:41 -0200

    Removed middle import

--- .floy/floy/managers/boilerplate.py
+++ .floy/floy/managers/boilerplate.py
@@ -2,20 +2,21 @@ from fabric.api import *
 from fabric.colors import *
 from abc import ABCMeta, abstractmethod
 
+
 class ManagerBoilerplate(object):
-  __metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-  def __init__(self):
-    if(len(env.tasks) < 2):
-      print red('>> Environment was set, but no task was specified.')
-      exit(1)
+    def __init__(self):
+        if(len(env.tasks) < 2):
+            print red('>> Environment was set, but no task was specified.')
+            exit(1)
 
-  @abstractmethod
-  def setup():
-    ''' Must override '''
-    pass
+    @abstractmethod
+    def setup():
+        ''' Must override '''
+        pass
 
-  def __getattr__(self, name):
-    def _missing(*args, **kwargs):
-      print red('>> This environment (%s) has no task named "%s"' % (env.name, name))
-    return _missing
+    def __getattr__(self, name):
+        def _missing(*args, **kwargs):
+            print red('>> This environment (%s) has no task named "%s"' % (env.name, name))
+        return _missing
--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,10 @@
+"""Floy fabfile"""
 # coding: utf-8
+
 import os
 import sys
 import inspect
+import importlib
 
 '''
 Adds the .floy/floy directory to python's include path.
@@ -12,17 +15,18 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '.floy/'))
 '''
 Importing floy module
 '''
-import floy
+FLOY = importlib.import_module('floy')
+
 
 '''
 Initializes floy
 '''
 
-floy.Core.init(os.path.dirname(__file__))
-for method in inspect.getmembers(floy.Tasks):
-  if not method[0].startswith('_'):
-    setattr(
-        __import__(__name__),
-        method[0],
-        getattr(floy.Tasks, method[0])
-    )
+FLOY.Core.init(os.path.dirname(__file__))
+for method in inspect.getmembers(FLOY.Tasks):
+    if not method[0].startswith('_'):
+        setattr(
+            __import__(__name__),
+            method[0],
+            getattr(FLOY.Tasks, method[0])
+        )
